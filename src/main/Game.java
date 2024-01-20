33093c2ea19a2b28fcba0b45cbdd7096d1b18a19
Marco <marco10@live.at>
2024-01-20 14:30:13 +0100

    created main classes and a window

---
+++ src/main/Game.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Game {
+
+    private GamePanel gamePanel;
+    private GameWindow gameWindow;
+    // Main Method
+    public Game() {
+        gamePanel = new GamePanel();
+        gameWindow = new GameWindow(gamePanel);
+    }
+
+}
